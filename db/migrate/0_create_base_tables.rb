b9a76aca7eb894b3849f19f1f5726f919ff705fd
Ariel Fogel <fogeltine@gmail.com>
2017-06-07 19:31:00 +0300

    change base tables to be account and roles

--- db/migrate/0_create_base_tables.rb
+++ db/migrate/0_create_base_tables.rb
@@ -1,33 +1,15 @@
 class CreateBaseTables < ActiveRecord::Migration
-
   def change
-    create_table :users do |t|
+    create_table :accounts do |t|
       t.string :username
-      t.string :avatar_url
       t.string :email
-      t.string :password
+      t.string :hashed_password
       t.timestamps
     end
 
-    create_table :posts do |t|
-      t.references :user
-      t.string :photo_url
+    create_table :roles do |t|
+      t.references :accounts
       t.timestamps
     end
-
-    create_table :comments do |t|
-      t.references :user
-      t.references :post
-      t.text :text
-      t.timestamps
-    end
-
-    create_table :likes do |t|
-      t.references :user
-      t.references :post
-      t.timestamps
-    end
-
   end
-
 end
